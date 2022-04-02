499adda02efe11cebdc58a6d4eb3235df2dbbce3
Shrey Banga <banga.shrey@gmail.com>
2022-04-02 01:40:07 -0700

    Switch to starring
    This updated implementation now uses starring as a signal for subscribing to files and folders, to cut down noise from irrelevant docs.
    
    It recursively searches for updates in starred folders, so this may become too slow over time.

--- code.js
+++ code.js
@@ -1,112 +1,83 @@
 /**
- * Sends a digest email of all items updated in the drive since yesterday
+ * Sends a digest email of starred items updated in google drive since yesterday
  */
-const SEPARATOR = "&nbsp;&bull;&nbsp;";
+const yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
 
-function sendFeedDigest() {
-  // Fetch all the files since yesterday
-  const yesterday = new Date();
-  yesterday.setDate(yesterday.getDate() - 1);
+function* toESIterator(iterator) {
+  while (iterator.hasNext()) {
+    yield iterator.next();
+  }
+}
 
-  const params = `modifiedDate > "${yesterday.toISOString()}"`;
-  const files = DriveApp.searchFiles(params);
-  const fileObjs = [];
-  while (files.hasNext()) {
-    const file = files.next();
+function formatFile(file) {
+  return `<div><a style="text-decoration: none" href="${file.getUrl()}">${file.getName()}</a></div>`;
+}
 
-    const folderIterator = file.getParents();
-    const folders = [];
-    while (folderIterator.hasNext()) {
-      folders.push(folderIterator.next());
-    }
+function getUpdatesFromFolder(folder, level = 0) {
+  Logger.log(''.padStart(level, ' ') + folder.getName());
 
-    fileObjs.push({
-      name: file.getName(),
-      url: file.getUrl(),
-      owner: file.getOwner(),
-      folders,
+  const updates = [];
+  for (const childFolder of toESIterator(folder.getFolders())) {
+    updates.push(...getUpdatesFromFolder(childFolder, level + 1));
+  }
+
+  const files = [...toESIterator(folder.getFiles())].filter(file => file.getLastUpdated() > yesterday);
+  if (files.length > 0 || updates.length > 0) {
+    // If there are any direct or indirect updates, include the folder
+    updates.unshift({
+      label: folder.getName(),
+      url: folder.getUrl(),
+      files,
+      level
     });
   }
 
-  // Group by folder names
-  const fileObjsByFolderIds = new Map();
-  for (const fileObj of fileObjs) {
-    const { folders } = fileObj;
-    const folderIds = folders
-      .map((folder) => folder.getId())
-      .sort()
-      .join(",");
-    if (!fileObjsByFolderIds.has(folderIds)) {
-      fileObjsByFolderIds.set(folderIds, []);
-    }
-    fileObjsByFolderIds.get(folderIds).push(fileObj);
-  }
-
-  // Sort by number of items
-  const items = Array.from(fileObjsByFolderIds.values());
-  items.sort((a, b) => b.length - a.length);
-
-  const itemsHtml = items
-    .map((fileObjs) => {
-      const folders = fileObjs[0].folders;
-      const folderPaths = folders.flatMap(getFolderPaths_);
-      const folderDescription =
-        folderPaths
-          .map((folderPath) => folderPath.join(" &gt; "))
-          .join("<br/>") || "(No name)";
-      return `
-        <div style="margin-bottom: 15px">
-          <span style="font-size: 11px; color: #666">${folderDescription}</span>
-            ${fileObjs
-              .map(
-                ({ name, url, owner }) => `
-                    <div style="margin-bottom: 5px;">
-                        <a style="font-size: 14px; text-decoration: none" href="${url}">${name}</a>
-                        ${SEPARATOR}
-                        ${
-                          owner
-                            ? `<span style="font-size: 13px">${owner.getName()}</span>`
-                            : ""
-                        }
-                    </div>
-                `
-              )
-              .join("")}
-        </div>`;
-    })
-    .join("");
-
-  const htmlBody = `
-    <div style="padding:20px 50px;">
-      <h2>Updates since ${yesterday.toLocaleDateString("en-US", {
-        weekday: "long",
-        month: "long",
-        day: "numeric",
-      })}</h2>
-      ${itemsHtml}
-    </div>
-  `;
-
-  MailApp.sendEmail(
-    "shrey.banga@airtable.com",
-    "Google drive updates",
-    "See HTML body",
-    {
-      name: "Google Drive",
-      htmlBody,
-    }
-  );
+  return updates;
 }
 
-function getFolderPaths_(folder) {
-  const paths = [];
-  const parents = folder.getParents();
-  const folderName = folder.getName();
-  while (parents.hasNext()) {
-    const parent = parents.next();
-    for (const parentPath of getFolderPaths_(parent)) {
-      paths.push([...parentPath, folderName]);
+function sendFeedDigest() {
+  // Find all starred files updated since yesterday
+  const starredFiles = DriveApp.searchFiles(`modifiedDate > "${yesterday.toISOString()}" and starred=true`);
+  const updates = [
+    {
+      label: 'Starred',
+      url: 'https://drive.google.com/drive/starred',
+      files: [...toESIterator(starredFiles)],
+      level: 0
     }
+  ];
+
+  // Add updated files from starred folders
+  const starredFolders = toESIterator(DriveApp.searchFolders('starred=true'));
+  for (const folder of starredFolders) {
+    updates.push(...getUpdatesFromFolder(folder));
   }
-  return paths.length > 0 ? paths : [[folderName]];
+
+  const itemsHtml = updates
+    .map(({ label, url, files, level }) => {
+      return `
+         <div style="margin: 1em 0 0 ${level}em">
+           <div style="font-size: 1em;"><a href="${url}" style="text-decoration: none; font-weight: bold">${label}</a></div>
+           <div style="font-size: 1.1em;">${files.map(formatFile).join('')}</div>
+         </div>
+       `
+    }).join('');
+
+  const htmlBody = `
+     <div style="color: #333; font-size: 12px">
+       ${itemsHtml}
+     </div>
+   `;
+
+  const numUpdates = updates.map(({ files }) => files.length).reduce((x, y) => x + y, 0);
+  const subject = `${numUpdates} Google Drive updates`;
+
+  MailApp.sendEmail({
+    to: "shrey.banga@airtable.com",
+    subject,
+    body: "See HTML body",
+    name: "📬 google drive",
+    htmlBody,
+  });
 }
